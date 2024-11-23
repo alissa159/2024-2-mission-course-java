d9e269af7094423557991ee0bc0f254f269871f4
Yun Seohyun <seohyunyun325@gmail.com>
2024-11-23 23:41:08 +0900

    GameCharacter

---
+++ src/main/java/com/gdsc/game/character/GameCharacter.java
@@ -0,0 +1,84 @@
+package com.gdsc.game.character;
+
+import com.gdsc.game.skill.Skill;
+import com.gdsc.game.skill.SkillAction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameCharacter implements CharacterAction {
+    private String name;
+    private int hp;
+    private int mp;
+    private int defense;
+    private List<SkillAction> skills;
+    private Random random;
+
+    public GameCharacter(String name, int hp, int mp) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.defense = 0;
+        this.random = new Random();
+        this.skills = new ArrayList<>();
+        initSkills();
+    }
+
+    private void initSkills() {
+        skills.add(new Skill("CuttingTwice", 2, 0));
+        skills.add(new Skill("CuttingThreeTimes", 3, 0));
+        skills.add(new Skill("HittingHard", 5, 2));
+    }
+
+    @Override
+    public int attack() {
+        return random.nextInt(10) + 1;
+    }
+
+    @Override
+    public void defend() {
+        defense = random.nextInt(10) + 1;
+    }
+
+    @Override
+    public int useSkill(int index) {
+        SkillAction skill = skills.get(index);
+        int damage = skill.use(mp);
+        if (damage > 0) {
+            mp -= skill.getManaCost();
+        }
+        return damage;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        hp -= Math.max(0, damage - defense);
+        defense = 0;
+    }
+
+    @Override
+    public void updateSkills() {
+        skills.forEach(SkillAction::reduceCooldown);
+    }
+
+    @Override
+    public String getStatus(CharacterAction other) {
+        return String.format("%s hp : %d md: %d | %s hd: %d md: %d\n" +
+                        "1. Attack(1 ~ 10)\n" +
+                        "2. Defend(1 ~ 10)\n" +
+                        "3. %s\n4. %s\n5. %s",
+                name, hp, mp, other.getName(), other.getHp(), mp,
+                skills.get(0).getInfo(), skills.get(1).getInfo(), skills.get(2).getInfo());
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getHp() {
+        return hp;
+    }
+}
