71a496d1d1ebf08c4e9239f4916ff61d7cb81f9e
Yun Seohyun <seohyunyun325@gmail.com>
2024-12-26 23:51:32 +0900

    코드 수정 위해 삭제한 클래스들

--- src/main/java/com/gdsc/game/skill/SkillAction.java
+++
@@ -1,9 +0,0 @@
-package com.gdsc.game.skill;
-
-public interface SkillAction {
-    boolean canUse(int mana);
-    int use(int mana);
-    void reduceCooldown();
-    String getInfo();
-    int getManaCost();
-}
